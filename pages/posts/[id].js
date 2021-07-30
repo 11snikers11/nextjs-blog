73f1f62e5242d913a4ac5850c49fe5a0aab8f886
Дмитрий Косяков <kd131150@gmail.com>
2021-07-30 18:32:36 +0300

    Инициализация

---
+++ pages/posts/[id].js
@@ -0,0 +1,28 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostsData } from '../../lib/posts';
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostsData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
